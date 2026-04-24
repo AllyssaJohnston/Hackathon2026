570d4aee188eb4995269583f89957e19461b718a
AllyssaJohnston <168232004+AllyssaJohnston@users.noreply.github.com>
2026-04-24 15:42:16 -0700

    code clean up

--- Hackathon2026/Assets/Scripts/Equation.cs
+++ Hackathon2026/Assets/Scripts/Equation.cs
@@ -3,45 +3,42 @@ using UnityEngine;
 public class Equation : MonoBehaviour
 {
     [SerializeField] GameObject gridInputSpot;
-    public Block inputVar;
+    [SerializeField] Block inputVar;
     [SerializeField] Block constVar;
     [SerializeField] EOp operation;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        inputVar = GetComponent<Block>();
         gridInputSpot.GetComponent<SpriteRenderer>().color = Color.green;
-        Debug.Log("changed color");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    public void setInputVar(Block var) { inputVar = var; }
 
     public void Compute()
     {
         switch (operation)
         {
-            case EOp.EOp_ADD:
-                inputVar.changeVal(inputVar.getVal() + constVar.getVal());
-                break;
-            case EOp.EOp_SUB:
-                inputVar.changeVal(inputVar.getVal() - constVar.getVal());
-                break;
-            case EOp.EOp_MUL:
-                inputVar.changeVal(inputVar.getVal() * constVar.getVal());
-                break;
-            case EOp.EOp_DIV:
-                inputVar.changeVal(inputVar.getVal() / constVar.getVal());
-                break;
-            case EOp.EOp_MOD:
-                inputVar.changeVal(inputVar.getVal() % constVar.getVal());
-                break;
-            default:
-                Debug.Log("unknown op" + operation);
-                break;
+        case EOp.EOp_ADD:
+            inputVar.changeVal(inputVar.getVal() + constVar.getVal());
+            break;
+        case EOp.EOp_SUB:
+            inputVar.changeVal(inputVar.getVal() - constVar.getVal());
+            break;
+        case EOp.EOp_MUL:
+            inputVar.changeVal(inputVar.getVal() * constVar.getVal());
+            break;
+        case EOp.EOp_DIV:
+            inputVar.changeVal(inputVar.getVal() / constVar.getVal());
+            break;
+        case EOp.EOp_MOD:
+            inputVar.changeVal(inputVar.getVal() % constVar.getVal());
+            break;
+        default:
+            Debug.Log("unknown op" + operation);
+            break;
         }
 
     }
